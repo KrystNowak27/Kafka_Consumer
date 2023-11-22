fb04d1b742df72a6072ff7b50fc2a6f8138fca83
Krystian <krystian.nowak27@gmail.com>
2023-11-22 12:32:27 +0100

    Add docker container and kafka

--- src/main/java/org/nowak/service/DatabaseCleanupScheduler.java
+++ src/main/java/org/nowak/service/DatabaseCleanupScheduler.java
@@ -1,11 +1,9 @@
 package org.nowak.service;
-
 import org.nowak.repository.ClientSpringDataJPARepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
