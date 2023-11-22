cb4418c8a42bb37798879c7c48ca0440939da61b
Krystian <krystian.nowak27@gmail.com>
2023-11-22 12:23:23 +0100

    Add docker container and kafka

--- src/main/java/org/nowak/service/DatabaseCleanupScheduler.java
+++ src/main/java/org/nowak/service/DatabaseCleanupScheduler.java
@@ -1,17 +1,26 @@
 package org.nowak.service;
 
 import org.nowak.repository.ClientSpringDataJPARepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 @Component
 public class DatabaseCleanupScheduler {
+
     private ClientSpringDataJPARepository repository;
 
+    @Autowired
+    public DatabaseCleanupScheduler(ClientSpringDataJPARepository repository) {
+        this.repository = repository;
+    }
+
     @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
     public void cleanupOldRecords() {
         LocalDateTime oneMonthAgo = LocalDateTime.now().minus(1, ChronoUnit.MONTHS);
         repository.deleteByCreatedAtBefore(oneMonthAgo);
